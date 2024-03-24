bddedfcad1bc8a8d97d8fba4a5b3c915656e504b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-24 21:21:34 +0100

    działa

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
 from markupsafe import escape
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=Rafal)
+    if name:
+        name = escape(name)  # Zabezpiecz przed potencjalnymi atakami XSS
+    else:
+        name = "Rafał"  # Domyślne imię, jeśli nie podano parametru
+    return render_template('hello.html', name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
